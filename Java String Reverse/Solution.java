1cf7845c3245da1d6b1985367fd6e9c0fbb29b9a
Sishir Pokhrel <35718069+pokhrelsishir@users.noreply.github.com>
2018-12-25 11:36:51 -0600

    Add files via upload

--- Java String Reverse/Solution.java
+++ Java String Reverse/Solution.java
@@ -7,21 +7,17 @@ public class Solution {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
-        String result = "";
+        String result = "Yes";
 
         String A=sc.next();
 
         for (int i = 0; i < A.length()/2; i++)
         {
-            if(A.charAt(i) == (A.charAt(A.length()-1-i)))
-            {
-                result = "Yes";
-            }
-
-            else
+            if(A.charAt(i) != (A.charAt(A.length()-1-i)))
             {
                 result = "No";
             }
+
 
         }
 
